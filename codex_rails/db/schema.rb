210da97ea9da12684ee07b7922e586fbc3a11231
Everton Silva <thonns@gmail.com>
2018-02-24 17:09:15 -0300

    chore: add column 'status' to books table

--- codex_rails/db/schema.rb
+++ codex_rails/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180224185607) do
+ActiveRecord::Schema.define(version: 20180224200807) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20180224185607) do
     t.datetime "updated_at", null: false
     t.integer "author_id"
     t.integer "price_group_id"
+    t.boolean "status"
   end
 
   create_table "books_categories", id: false, force: :cascade do |t|
