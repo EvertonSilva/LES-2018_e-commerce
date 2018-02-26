f932d398fef2f07b85a3b8564cb84e9e4d654476
Everton Silva <thonns@gmail.com>
2018-02-25 23:45:14 -0300

    test: move integration tests to controller tests

--- codex_rails/test/integration/book_requests_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class BookRequestsTest < ActionDispatch::IntegrationTest
-
-  test "end point GET /books" do
-    get books_url, as: :json
-    assert_response :success
-  end
-end
