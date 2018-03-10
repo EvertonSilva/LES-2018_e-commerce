486218b78db8ede6d84a9b981dc78b95373d1aff
Everton Silva <thonns@gmail.com>
2018-03-10 20:16:27 -0300

    fix: typo on constructor method

--- codex_rails/app/models/dimensions.rb
+++ codex_rails/app/models/dimensions.rb
@@ -2,7 +2,7 @@ class Dimensions
   attr_reader :width, :height, :weight, :depth
 
   def initialize(width, height, weight, depth)
-    @width, @height, @weight, @dept = width, height, weight, depth
+    @width, @height, @weight, @depth = width, height, weight, depth
   end
 
 end
