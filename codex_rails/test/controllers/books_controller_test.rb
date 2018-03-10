0b4673732aa26560540d492d8eb9c29c8ba6f110
Everton Silva <thonns@gmail.com>
2018-03-10 17:47:58 -0300

    feature: return books json

--- codex_rails/test/controllers/books_controller_test.rb
+++ codex_rails/test/controllers/books_controller_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class BooksControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @books_list = [books(:one), books(:two), books(:three)]
+    @books_list.each { |book| book.save }
+  end
+
+  def teardown
+    @books_list.each { |book| book.destroy }
+  end
+
+  test "end point GET /books" do
+    get books_url, as: :json
+    assert_response :success
+  end
+
+  test "return json with all books" do
+    get books_url, as: :json
+    payload = JSON.parse(response.body)
+    puts payload.class
+    assert_equal 5, payload.count
+  end
+
 end
