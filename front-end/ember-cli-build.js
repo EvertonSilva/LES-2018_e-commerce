77159236e5a53f1e8ff829f324a0a8bd114ca7cb
Everton Silva <thonns@gmail.com>
2018-03-10 22:50:34 -0300

    chore: config ember-cli-sass addon

--- front-end/ember-cli-build.js
+++ front-end/ember-cli-build.js
@@ -5,6 +5,9 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     // Add options here
+    sassOptions: {
+      extension: "sass"
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
