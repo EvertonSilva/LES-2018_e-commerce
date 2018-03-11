2d368e175dcf478269a225a9baee6e0c1ec65ae5
Everton Silva <thonns@gmail.com>
2018-03-11 00:42:35 -0300

    feature: list books using model object for books page

--- front-end/app/routes/books.js
+++ front-end/app/routes/books.js
@@ -1,4 +1,32 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model() {
+    return [
+      {
+        title: "Crime and Punishment",
+        author: "Dostoievski, Fiodor",
+        price: 0.00,
+        pages: 180,
+        publisher: "Editora 34",
+        categories: ["Drama", "Fiction"]
+      }
+      ,{
+        title: "The Demons",
+        author: "Dostoievski, Fiodor",
+        price: 0.00,
+        pages: 250,
+        publisher: "Editora 34",
+        categories: ["Drama", "Fiction"]
+      }
+      ,{
+        title: "Notes from the Underground",
+        author: "Dostoievski, Fiodor",
+        price: 0.00,
+        pages: 100,
+        publisher: "Editora 34",
+        categories: ["Drama", "Fiction"]
+      }
+    ]
+  }
 });
