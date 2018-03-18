2b5b072a303a4175266f425ef69aee7475ad5b7c
Everton Silva <thonns@gmail.com>
2018-03-18 11:36:28 -0300

    feature: handle post request

--- codex_rails/app/controllers/books_controller.rb
+++ codex_rails/app/controllers/books_controller.rb
@@ -3,4 +3,20 @@ class BooksController < ApplicationController
     @books = Book.all
     render json:  @books
   end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      response = { data: { id: @book.id, type: @book.class.name.downcase.pluralize } }
+    else
+      response = { errors: @book.errors }
+    end
+
+    render json: response.to_json
+  end
+
+  private
+    def book_params
+      params.require(:book).permit(:title, :isbn, :barcode, :status, :synopsis, :author_id, :price_group_id, :edition_id)
+    end
 end
