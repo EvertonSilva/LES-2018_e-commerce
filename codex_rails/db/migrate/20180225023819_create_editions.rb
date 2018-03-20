585d305576cc3bbf36825078711acc8eecca5dc9
Everton Silva <thonns@gmail.com>
2018-03-20 20:20:48 -0300

    chore: add domain data.
    Use seed file to insert data into some of domain tables to get a set of data to support CRUD operation for book.

--- codex_rails/db/migrate/20180225023819_create_editions.rb
+++ codex_rails/db/migrate/20180225023819_create_editions.rb
@@ -3,10 +3,10 @@ class CreateEditions < ActiveRecord::Migration[5.1]
     create_table :editions do |t|
       t.date :publish_year
       t.integer :pages_number
-      t.string :width
-      t.string :height
-      t.string :weight
-      t.string :depth
+      t.integer :width
+      t.integer :height
+      t.integer :weight
+      t.integer :depth
       t.references :publisher, foreign_key: true
 
       t.timestamps
--- codex_rails/db/seeds.rb
+++ codex_rails/db/seeds.rb
@@ -5,3 +5,50 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# create authors
+authors_list = [
+  { name: 'Dijkstra, Edsger'},
+  { name: 'Ritchie, Dennis'},
+  { name: 'Tolkien, J.R.R.'},
+  { name: "Matsumoto, Yukihiro 'Matz'"},
+  { name: 'Hansson, David H.'},
+  { name: 'Gosling, James'},
+  { name: 'Dostoiévski, Fiodor'},
+  { name: 'Palahniuk, Chuck'},
+  { name: 'Sarte, Jean P.' }
+]
+Author.create(authors_list)
+
+# create categories
+categories_list = [
+  { name: 'Computer Science', desc: 'lorem ipsum' },
+  { name: 'Fantasy & Fiction', desc: 'lorem ipsum'},
+  { name: 'Terror', desc: 'lorem ipsum'},
+  { name: 'Sci Fi', desc: 'lorem ipsum'}
+]
+Category.create(categories_list)
+
+# create price price groups
+price_groups = [
+  { desc: "pg1", markup: 5 },
+  { desc: "pg2", markup: 10 },
+  { desc: "pg3", markup: 15 },
+  { desc: "pg4", markup: 25 },
+  { desc: "pg5", markup: 30 },
+]
+PriceGroup.create(price_groups)
+
+# create publishers
+publishers_list = [
+  { name: "Editora 34" },
+  { name: "Novatec" },
+  { name: "Booksman" },
+  { name: "Leya" },
+  { name: "Cia das Letras" },
+]
+Publisher.create(publishers_list)
+
+# create edition
+dim  = Dimensions.new(12, 17, 180, 8)
+Edition.create({ publish_year: Time.new(2000), pages_number: 200, dimensions: dim, publisher_id: 1 })
