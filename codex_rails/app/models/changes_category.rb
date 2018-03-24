7ffe6f9f53e336ce90fbe3b296ac25e6a9f3998d
Everton Silva <thonns@gmail.com>
2018-03-24 19:32:17 -0300

    feature: performe data validation
    Use Active Record's validation feature to ensure data integrity before saving into database.

--- codex_rails/app/models/changes_category.rb
+++ codex_rails/app/models/changes_category.rb
@@ -1,4 +1,7 @@
 class ChangesCategory < ApplicationRecord
 
+  validates_presence_of :name, :change_type
+  validates_associated :status_changes
+  
   has_many :status_changes
 end
--- codex_rails/app/models/status_change.rb
+++ codex_rails/app/models/status_change.rb
@@ -1,4 +1,8 @@
 class StatusChange < ApplicationRecord
+
+  validates_presence_of :reason
+  validates_associated :changes_category
+
   belongs_to :book
   belongs_to :changes_category
 end
