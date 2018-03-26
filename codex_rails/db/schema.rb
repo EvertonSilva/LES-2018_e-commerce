1092596245d4856df9d891ba65aa21f67f7e3443
Everton Silva <thonns@gmail.com>
2018-03-26 02:33:50 -0300

    chore: run paper_trail:install to config the gem

--- codex_rails/db/schema.rb
+++ codex_rails/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180324172910) do
+ActiveRecord::Schema.define(version: 20180326053251) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,6 +88,16 @@ ActiveRecord::Schema.define(version: 20180324172910) do
     t.datetime "updated_at", null: false
     t.index ["book_id"], name: "index_status_changes_on_book_id"
     t.index ["changes_category_id"], name: "index_status_changes_on_changes_category_id"
+  end
+
+  create_table "versions", force: :cascade do |t|
+    t.string "item_type", null: false
+    t.integer "item_id", null: false
+    t.string "event", null: false
+    t.string "whodunnit"
+    t.text "object"
+    t.datetime "created_at"
+    t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
   end
 
   add_foreign_key "books", "authors"
