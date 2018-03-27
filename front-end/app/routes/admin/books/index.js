6d8c4de7fa160686273f1d428c804c43b63e361c
Everton Silva <thonns@gmail.com>
2018-03-27 12:47:35 -0300

    refactor(front-end): include authors on books request

--- front-end/app/routes/admin/books/index.js
+++ front-end/app/routes/admin/books/index.js
@@ -2,6 +2,6 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model() {
-    return this.get('store').findAll('book');
+    return this.get('store').findAll('book', { include: 'author' });
   }
 });
