bfd086220a64b1b0a7055e71b840fc9ae81e99fd
Everton Silva <thonns@gmail.com>
2018-03-27 12:50:41 -0300

    feature(rails-api): retrieve book categories

--- codex_rails/app/controllers/categories_controller.rb
+++ codex_rails/app/controllers/categories_controller.rb
@@ -1,2 +1,16 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: [:show]
+
+  def index
+    render json: Category.all
+  end
+
+  def show
+    render json: @category
+  end
+
+  private
+  def set_category
+    @category = Category.find params[:id]
+  end
 end
