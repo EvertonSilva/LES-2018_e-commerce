51d55ffd9b6cecb57d75037a390ae93e23983d89
Everton Silva <thonns@gmail.com>
2018-03-27 12:01:21 -0300

    feature: define edition resource

--- codex_rails/app/controllers/editions_controller.rb
+++ codex_rails/app/controllers/editions_controller.rb
@@ -1,2 +1,15 @@
 class EditionsController < ApplicationController
+  before_action :set_edition, only: [:show, :update]
+  def index
+    render json: Edition.all
+  end
+
+  def show
+    render json: @edition
+  end
+
+  private
+  def set_edition
+    @edition = Edition.find params[:id]
+  end
 end
--- codex_rails/config/routes.rb
+++ codex_rails/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :books
-  get 'authors/:id', to: 'authors#show'
-  get 'authors/', to: 'authors#index'
+  resources :books do
+    resources :editions
+  end
+  resources :authors
 end
