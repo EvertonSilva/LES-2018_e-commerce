06728752f1d7c289f0231282f6c1787693d3a08f
Everton Silva <thonns@gmail.com>
2018-03-26 22:19:12 -0300

    feature: listing books from api response

--- front-end/app/routes/admin/books/index.js
+++ front-end/app/routes/admin/books/index.js
@@ -2,34 +2,6 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model() {
-    return [
-      {
-        id: 1,
-        title: "Crime and Punishment",
-        author: "Dostoievski, Fiodor",
-        price: 0.00,
-        pages: 180,
-        publisher: "Editora 34",
-        categories: ["Drama", "Fiction"]
-      }
-      ,{
-        id: 2,
-        title: "The Demons",
-        author: "Dostoievski, Fiodor",
-        price: 0.00,
-        pages: 250,
-        publisher: "Editora 34",
-        categories: ["Drama", "Fiction"]
-        }
-      ,{
-        id: 3,
-        title: "Notes from the Underground",
-        author: "Dostoievski, Fiodor",
-        price: 0.00,
-        pages: 100,
-        publisher: "Editora 34",
-        categories: ["Drama", "Fiction"]
-      }
-    ]
+    return this.get('store').findAll('book');
   }
 });
