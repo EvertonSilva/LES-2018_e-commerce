56a9c70068348539a8ae5c37329c9e64afce4c22
Everton Silva <thonns@gmail.com>
2018-03-27 14:49:14 -0300

    fix(rails-api): routes for categories and category serialization

--- codex_rails/config/routes.rb
+++ codex_rails/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'publishers', to: 'publishers#index'
   get 'publishers/:id', to: 'publishers#show'
+  get 'categories', to: 'categories#index'
+  get 'categories/:id', to: 'categories#show'
 
   resources :books do
     resources :editions
