577559b9be25ab46d294cb5ddea7d2922300b513
Everton Silva <thonns@gmail.com>
2018-03-27 01:27:20 -0300

    feature: add authors controller to interact with EmberData

---
+++ codex_rails/app/controllers/authors_controller.rb
@@ -0,0 +1,21 @@
+class AuthorsController < ApplicationController
+  before_action :set_author, only: [:update, :show]
+
+  def index
+  end
+
+  def show
+    render json: @author
+  end
+
+  private
+    def set_author
+      begin
+        @author = Author.find(params[:id])
+      rescue ActiveRecord::RecordNotFound
+        author = Author.new
+        author.errors.add(:id, "ID not found")
+        render_error(author, 404) and return
+      end
+    end
+end
