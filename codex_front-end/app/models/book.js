a028fac14ff88942b7ee303c5044ccdb08c4f17b
Everton Silva <thonns@gmail.com>
2018-04-07 19:43:25 -0300

    chore(front-end): generate book model

---
+++ codex_front-end/app/models/book.js
@@ -0,0 +1,18 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  title: DS.attr('string'),
+  isbn: DS.attr('string'),
+  barcode: DS.attr('string'),
+  synopsis: DS.attr('string'),
+  edtion: DS.attr('string'),
+  width: DS.attr('number'),
+  height: DS.attr('number'),
+  weight: DS.attr('number'),
+  depth: DS.attr('number'),
+  author: DS.belongsTo('author'),
+  publisher: DS.belongsTo('publisher'),
+  priceGroup: DS.belongsTo('price-group'),
+  statusChanges: DS.hasMany('status-change'),
+  categories: DS.hasMany('category'),
+});
