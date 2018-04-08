4e314b2056768dc3c56e2c94c65fd78c8ed0a6ff
Everton Silva <thonns@gmail.com>
2018-04-08 15:15:33 -0300

    fix(rails-api): validate book page numbers presence

--- codex_rails/app/models/book.rb
+++ codex_rails/app/models/book.rb
@@ -2,7 +2,7 @@ class Book < ApplicationRecord
   before_create :set_status
   # validations
   validates_presence_of :title, :isbn, :barcode, :synopsis, :edition,
-                        :width, :height, :weigth, :depth
+                        :page_numbers, :width, :height, :weight, :depth
   # associations
   has_and_belongs_to_many :categories
   has_many :status_changes
