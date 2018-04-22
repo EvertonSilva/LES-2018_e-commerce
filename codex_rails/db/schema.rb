597a0bcb85460c77c2ed5306dd87fe178ed8d875
Everton Silva <thonns@gmail.com>
2018-04-22 20:46:24 -0300

    chore(rails-api): create table credit_cards and model class

--- codex_rails/db/schema.rb
+++ codex_rails/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180422224729) do
+ActiveRecord::Schema.define(version: 20180422234617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,14 @@ ActiveRecord::Schema.define(version: 20180422224729) do
   create_table "changes_categories", force: :cascade do |t|
     t.string "name"
     t.integer "change_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "credit_cards", force: :cascade do |t|
+    t.string "card_number"
+    t.string "printed_name"
+    t.string "security_code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
