0065cc01e1b6b45a99abd16d02e84eea30dcb6cb
Everton Silva <thonns@gmail.com>
2018-04-21 22:15:52 -0300

    refactor(front-end): use component properties to deal with corner cases.

--- codex_front-end/app/components/book-select-collection.js
+++ codex_front-end/app/components/book-select-collection.js
@@ -1,4 +1,24 @@
 import Component from '@ember/component';
+import { observer } from '@ember/object';
+import $ from 'jquery';
 
 export default Component.extend({
+  entity: null,
+  categoryIDs: null,
+
+  filterCategories: observer('categoryIDs', function() {
+  }),
+
+  actions: {
+    // handler for change event on form select tag
+    selectEntity(event) {
+      const selectedVal = $(event.target).val(); // get value for selected element
+      if(this.isMultiple) { // selected from multiple select tag?
+        this.set('categoryIDs', selectedVal); // store category IDs for latter process
+      } else {
+        this.set('entity', this.collection.find(item => item.id === selectedVal));
+        const modelName = this.isPriceGroup ? 'priceGroup':this.entity.constructor.modelName;
+      }
+    },
+  }
 });
