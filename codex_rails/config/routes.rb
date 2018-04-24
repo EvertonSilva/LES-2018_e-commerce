3eb45d2a4ea1a5a26693fc02b1bf6ae2cd542aae
Everton Silva <thonns@gmail.com>
2018-04-23 23:46:34 -0300

    feat(rails-api): customers create action

--- codex_rails/config/routes.rb
+++ codex_rails/config/routes.rb
@@ -8,4 +8,8 @@ Rails.application.routes.draw do
 
   resources :books
   resources :authors
+  resources :customers do
+    resources :credit_cards, shallow: true
+    resources :addresses, shallow: true
+  end
 end
---
+++ codex_rails/test/controllers/customers_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class CustomersControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get customers_index_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get customers_create_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get customers_update_url
+    assert_response :success
+  end
+
+end
