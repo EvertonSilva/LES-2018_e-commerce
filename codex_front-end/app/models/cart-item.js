456b4211a546b50327a69a24b82d51484ebd23d8
Everton Silva <thonns@gmail.com>
2018-05-13 15:09:44 -0300

    feat(front-end): total and subtotal from shopping cart

--- codex_front-end/app/models/cart-item.js
+++ codex_front-end/app/models/cart-item.js
@@ -1,8 +1,17 @@
-import EmberObject from '@ember/object';
+import EmberObject, { computed } from '@ember/object';
 
 export default EmberObject.extend({
   init() {
     this.set('book', null);
     this.set('amount', 0);
-  }
+    this.set('price', 0.0);
+  },
+
+  subtotal: computed('amount', 'price', function() {
+    let amount = this.get('amount');
+    let price = this.get('price');
+
+    return amount * price;
+  }),
+
 });
--- codex_front-end/app/services/shopping-cart.js
+++ codex_front-end/app/services/shopping-cart.js
@@ -1,4 +1,5 @@
 import Service from '@ember/service';
+import { computed } from '@ember/object';
 import CartItem from '../models/cart-item';
 
 export default Service.extend({
@@ -7,16 +8,24 @@ export default Service.extend({
     this.set('items', new Array());
   },
 
+  total: computed('items.@each.amount', function() {
+    let listItems = this.get('items');
+    let totalSum = 0;
+    listItems.forEach((item) => totalSum += item.get('subtotal'));
+    return totalSum.toFixed(2);
+  }),
+
   addItem(item) {
     let _item = this._findItem(item);
     if(undefined === _item) {
       let cartItem = new CartItem();
       cartItem.set('book', item);
       cartItem.set('amount', 1);
+      cartItem.set('price', this._getPrice());
       this.get("items").pushObject(cartItem);
     } else {
-      let amount = _item.get('amount');
-      _item.set('amount', amount + 1);
+      let currentAmount = _item.get('amount');
+      _item.set('amount', currentAmount + 1);
     }
   },
 
@@ -29,5 +38,10 @@ export default Service.extend({
 
   _findItem(lookUpItem) {
     return this.get('items').find((item) => item.get('book').get('id') === lookUpItem.get('id'));
-  }
+  },
+
+   _getPrice() {
+     const price = Math.random() * (100 - 10) + 10;
+     return price.toFixed(2);
+   }
 });
