90c9b842855c32e04fb2473c539acf621153eccc
Everton Silva <thonns@gmail.com>
2018-05-13 11:16:23 -0300

    feat(front-end): remove itens from shopping cart

--- codex_front-end/app/components/display-cart.js
+++ codex_front-end/app/components/display-cart.js
@@ -3,4 +3,11 @@ import { inject as service } from '@ember/service';
 
 export default Component.extend({
   shoppingCart: service(),
+
+  actions: {
+    removeFromCart(book) {
+      alert('this book will be removed from your cart');
+      this.get('shoppingCart').removeItem(book);
+    }
+  }
 });
--- codex_front-end/app/services/shopping-cart.js
+++ codex_front-end/app/services/shopping-cart.js
@@ -1,9 +1,10 @@
 import Service from '@ember/service';
+import EmberArray from '@ember/array';
 
 export default Service.extend({
   init() {
     this._super(...arguments);
-    this.set('items', new Array());
+    this.set('items', new EmberArray());
   },
 
   addItem(item) {
@@ -11,6 +12,7 @@ export default Service.extend({
   },
 
   removeItem(itemToRemove) {
+    // TODO: review this implementation
     let filteredItems = this.get('items').filter(
       (item) => item.get('id') !== itemToRemove.get('id')
     );
