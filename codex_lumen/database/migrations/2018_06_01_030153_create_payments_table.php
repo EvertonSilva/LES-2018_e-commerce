c0969533f6bcbbf8dba082a907147bfa36c48a7d
Everton Silva <thonns@gmail.com>
2018-06-10 20:34:52 -0300

    chore(sales-api): remove pay_method column from payment table

--- codex_lumen/database/migrations/2018_06_01_030153_create_payments_table.php
+++ codex_lumen/database/migrations/2018_06_01_030153_create_payments_table.php
@@ -15,9 +15,9 @@ class CreatePaymentsTable extends Migration
     {
         Schema::create('payments', function (Blueprint $table) {
             $table->increments('id');
-            $table->enum('pay_method', ['CREDIT_CARD']);
             $table->double('amount', 8, 2);
             $table->unsignedInteger('order_id');
+            $table->unsignedInteger('creditcard_id');
             $table->foreign('order_id')
                   ->references('id')->on('orders');
             $table->timestamps();
