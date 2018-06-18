003efa1a8d3b0d29c1b61da0827ae83538299329
Everton Silva <thonns@gmail.com>
2018-06-18 00:23:14 -0300

    feat(front-end): generate Order

--- codex_front-end/app/controllers/checkout.js
+++ codex_front-end/app/controllers/checkout.js
@@ -1,7 +1,5 @@
 import Controller from '@ember/controller';
-// import ShippingAddress from '../models/shipping-address';
 
 export default Controller.extend({
-  actions: {
-  }
+  promoCode: '',
 });
--- codex_front-end/app/routes/checkout.js
+++ codex_front-end/app/routes/checkout.js
@@ -1,4 +1,33 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+// import { hash } from 'rsvp';
 
 export default Route.extend({
+  order: null,
+  shoppingCart: service(),
+
+  getOrderlines() {
+    return this.get('shoppingCart').items.map(cartItem => {
+      let orderLine = this.get('store').createRecord('orderLine');
+      orderLine.set('amount', cartItem.get('amount'));
+      orderLine.set('sellPrice', cartItem.get('price'));
+      orderLine.set('book', cartItem.get('book'));
+      return orderLine;
+    });
+  },
+
+  beforeModel() {
+    if(this.get('order')) {
+      this.get('store').deleteRecord(this.get('order'));
+      this.set('order', null);
+    }
+  },
+
+  model() {
+    this.set('order', this.get('store').createRecord('order'));
+    this.get('order').set('orderLines', this.getOrderlines());
+    this.get('order').set('total', this.get('shoppingCart').get('total'));
+    return this.get('order');
+  },
+
 });
