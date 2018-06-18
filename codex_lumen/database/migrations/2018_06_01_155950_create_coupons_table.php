4177657ee4286c6ea68bcb987f3ae79f5d1e16c9
Everton Silva <thonns@gmail.com>
2018-06-18 01:00:38 -0300

    chore(salles-api): rename migration file

---
+++ codex_lumen/database/migrations/2018_06_01_155950_create_coupons_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCuponsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::create('coupons', function (Blueprint $table) {
+        $table->increments('id');
+        $table->string('code');
+        $table->enum('coupon_type', ['EXCHANGE', 'PROMO']);
+        $table->boolean('valid');
+        $table->decimal('discount');
+        $table->unsignedInteger('order_id');
+        $table->foreign('order_id')
+              ->references('id')->on('orders');
+        $table->timestamps();
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+      Schema::dropIfExists('coupons');
+    }
+}
