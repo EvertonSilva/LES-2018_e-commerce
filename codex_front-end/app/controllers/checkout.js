2db60b9a4c190f37af229388f82bd57a83d0d016
Everton Silva <thonns@gmail.com>
2018-06-18 11:03:55 -0300

    feat(front-end): calc shipping cost
    Calculate shipping cost when user select the address.

--- codex_front-end/app/controllers/checkout.js
+++ codex_front-end/app/controllers/checkout.js
@@ -2,4 +2,24 @@ import Controller from '@ember/controller';
 
 export default Controller.extend({
   promoCode: '',
+  orderTotal: 0.0,
+
+  actions: {
+    shippingAddressChange(value) {
+      let order = this.get('model');
+      let shippingCost = this.get('store').createRecord('shippingCost');
+      let cost = 0.0;
+      let purchaseTotal = parseFloat(order.get('total'));
+      if(value == 1) {
+        cost = 20.86;
+      } else if(value == 2) {
+        cost = 8.65;
+      } else {
+        cost = 0.0;
+      }
+      shippingCost.set('cost', cost);
+      order.set('shippingCost', shippingCost);
+      this.set('orderTotal', (purchaseTotal + cost).toFixed(2));
+    }
+  }
 });
