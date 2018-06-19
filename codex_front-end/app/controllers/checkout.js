5cef587363a86b3b93e0943173413ddb3cee43fc
Everton Silva <thonns@gmail.com>
2018-06-19 12:09:13 -0300

    feat(front-end): handle user interaction
    Define some event handlers to deal with credit card selection and exchange vouchers.

--- codex_front-end/app/controllers/checkout.js
+++ codex_front-end/app/controllers/checkout.js
@@ -1,15 +1,34 @@
 import Controller from '@ember/controller';
+import { computed } from '@ember/object';
+import $ from 'jquery';
+
+const creditCards = [];
 
 export default Controller.extend({
   promoCode: '',
-  orderTotal: 0.0,
+  orderDiscount: 0.0,
+  shippingCost: 0.0,
+  applyDiscount: false,
+  creditCards: null,
+  exchangeCoupons: null,
+
+  orderTotal: computed('shippingCost', 'orderDiscount', function() {
+    let discount = this.get('orderDiscount');
+    let shpCost = this.get('shippingCost');
+    let total = this.get('model').get('total');
+
+    return ((total - discount) + shpCost).toFixed(2);
+  }),
+
+  init() {
+    this._super(...arguments);
+    this.set('creditCards', []);
+    this.set('exchangeCoupons', []);
+  },
 
   actions: {
     shippingAddressChange(value) {
-      let order = this.get('model');
-      let shippingCost = this.get('store').createRecord('shippingCost');
       let cost = 0.0;
-      let purchaseTotal = parseFloat(order.get('total'));
       if(value == 1) {
         cost = 20.86;
       } else if(value == 2) {
@@ -17,9 +36,39 @@ export default Controller.extend({
       } else {
         cost = 0.0;
       }
-      shippingCost.set('cost', cost);
-      order.set('shippingCost', shippingCost);
-      this.set('orderTotal', (purchaseTotal + cost).toFixed(2));
+      this.set('shippingCost', cost);
+    },
+    couponCheck(value) {
+      if(value == "AB3021") {
+        this.set('orderDiscount', 25.65);
+        this.set('applyDiscount', true);
+      }
+    },
+    toggleCreditCard(event) {
+      const val = event.target.value;
+      if(event.target.checked) {
+        // add credit card to array of cards
+        this.get('creditCards').push(val);
+      } else {
+        // remove credit card from array of cards
+        const i = this.get('creditCards').indexOf(val);
+        this.get('creditCards').splice(i, 1);
+      }
+    },
+    toggleExchangeCoupons(event) {
+      const target = event.target;
+      const val = target.value;
+      const _discount = $(target).data('discount');
+      const data = { id: val, discount: _discount };
+
+      if(event.target.checked) {
+        // add coupons to array of coupons
+        this.get('exchangeCoupons').push(data);
+      } else {
+        // remove coupons from array of coupons
+        const i = this.get('exchangeCoupons').indexOf(data);
+        this.get('exchangeCoupons').splice(i, 1);
+      }
     }
   }
 });
