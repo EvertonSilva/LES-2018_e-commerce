c3d376f40c1fb11c9619697a8c2261416d434575
Everton Silva <thonns@gmail.com>
2018-06-19 12:14:18 -0300

    refactor(fron-end): add coloumn 'discount' to coupons.

--- codex_front-end/app/models/coupon.js
+++ codex_front-end/app/models/coupon.js
@@ -3,6 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   code: DS.attr(),
   coupon_type: DS.attr(),
+  discount: DS.attr(),
   valid: DS.attr('boolean'),
   order: DS.belongsTo('order')
 });
